9a408f476f0d26df826143a459b1fbfe20135e70
Alex Cui <acui@waabi.ai>
2022-10-13 22:28:39 +0000

    Add repro of the issue

---
+++ hydra_repro.py
@@ -0,0 +1,51 @@
+import hydra
+from hydra.core.config_store import ConfigStore
+from omegaconf import DictConfig, OmegaConf
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class TableConfig:
+    rows: int = 1
+    cols: int = 2
+
+@dataclass
+class DatabaseConfig:
+    driver: str = "postgresql"
+    user: str = "user"
+    password: str = "password"
+    table_cfg: TableConfig = TableConfig()
+
+@dataclass
+class ModelConfig:
+    data_source: Optional[TableConfig] = None
+    # data_source: TableConfig = TableConfig()
+
+@dataclass
+class ServerConfig:
+    db: DatabaseConfig
+    model: ModelConfig
+
+cs = ConfigStore.instance()
+cs.store(name="database", node=DatabaseConfig, group="db")
+cs.store(name="server", node=ServerConfig)
+
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def my_app(cfg : DictConfig) -> None:
+    """Should print:
+    db:
+        driver: postgresql
+        user: user
+        password: password
+        table_cfg:
+            rows: 1
+            cols: 2
+    model:
+        data_source: ${db.table_cfg}
+    """
+    print(OmegaConf.to_yaml(cfg))
+    # Should print 1
+    print(cfg.model.data_source.rows)
+
+if __name__ == "__main__":
+    my_app()
---
+++ hydra_success.py
@@ -0,0 +1,50 @@
+import hydra
+from hydra.core.config_store import ConfigStore
+from omegaconf import DictConfig, OmegaConf
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class TableConfig:
+    rows: int = 1
+    cols: int = 2
+
+@dataclass
+class DatabaseConfig:
+    driver: str = "postgresql"
+    user: str = "user"
+    password: str = "password"
+    table_cfg: TableConfig = TableConfig()
+
+@dataclass
+class ModelConfig:
+    rows: str
+
+@dataclass
+class ServerConfig:
+    db: DatabaseConfig
+    model: ModelConfig
+
+cs = ConfigStore.instance()
+cs.store(name="database", node=DatabaseConfig, group="db")
+cs.store(name="server", node=ServerConfig)
+
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def my_app(cfg : DictConfig) -> None:
+    """Should print:
+    db:
+        driver: postgresql
+        user: user
+        password: password
+        table_cfg:
+            rows: 1
+            cols: 2
+    model:
+        rows: ${db.table_cfg}
+    """
+    print(OmegaConf.to_yaml(cfg))
+    # Should print 1
+    print(cfg.model.rows)
+
+if __name__ == "__main__":
+    my_app()
